8f3f859d08bc0c7e995e211d76c4cd7efa2a9c31
chiariniolivia <c241@umbc.edu>
2024-01-21 18:07:08 -0500

    done but need to test alot of things

--- livova-installer.sh
+++ livova-installer.sh
@@ -115,13 +115,50 @@ if [ "$wm_check" == 0 ]; then
               sudo systemctl enable sddm
               ;;
             "$wm5")
-                echo "installing hyprland (non nvidia)"
-                
-
-
-                ;;
+              echo "installing hyprland (non nvidia)"
+              paru -S --needed --noconfirm gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus kitty wlroots python-pywlroots 
+              paru -S --needed --noconfirm hyprland-git 
+              paru -S --needed --noconfirm sddm
+              paru -S --needed --noconfirm wired wireplumber xdg-desktop-portal-hyprland polkit-kde-agent wofi
+            
+              sudo systemctl enable sddm
+              ;;
             "$wm6")
               echo "installing hyprland (nvidia)"
+              paru -S --needed --noconfirm gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus kitty wlroots python-pywlroots 
+              paru -S --needed --noconfirm hyprland-git 
+              paru -S --needed --noconfirm wired wireplumber xdg-desktop-portal-hyprland polkit-kde-agent wofi
+
+              #nvidia weirdness
+              paru -S --needed --noconfirm nvidia-dkms linux-headers qt5-wayland qt6-wayland qt5ct qt6ct libva libva-nvidia-driver-git
+
+              ##### test this
+              sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 nvidia_drm.modeset=1 noapic"/' /etc/default/grub
+              ##### test this
+              
+              sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+              ##### test this
+              sudo sed -i '/^MODULES=/ s/)$/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+              ##### test this
+              
+
+              sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
+
+              ##### test this
+              echo "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf > /dev/null
+              ##### test this
+
+              mkdir .config/hypr
+              cp repos/Hyprland/example/hyprland.conf .config/hypr/hyprland.conf
+              
+              ##### test this
+              echo -e "env = LIBVA_DRIVER_NAME,nvidia\nenv = XDG_SESSION_TYPE,wayland\nenv = GBM_BACKEND,nvidia-drm\nenv = __GLX_VENDOR_LIBRARY_NAME,nvidia\nenv = WLR_NO_HARDWARE_CURSORS,1" | tee -a ~/.config/hypr/config.hypr > /dev/null
+              ##### test this
+
+              paru -S --needed --noconfirm sddm
+              sudo systemctl enable sddm
+
               ;;
             
         esac
@@ -190,8 +227,87 @@ eval "paru -S --needed --noconfirm $packgelist"
 
 if [ "$prty" == 1 ]; then
   echo "installing recommended dot files"
+f [ "$wm_check" == 0 ]; then
+    case "$wmInst" in 
+            "$wm1" | "$wm3")
+              echo "installing gnome-dots"
+              cd ~/repos/
+              git clone https://github.com/GabrielTenma/dotfiles-gnm.git
+              cd dotfiles-gnm
+
+              cp -r ./themes/* ~/.themes/
+              cp -r ./icons/* ~/.icons/
+              cp -r ./gnome-shell/* ~/.local/share/gnome-shell/*
+
+              paru -S --needed --noconfirm gnome-tweak-tool
+              sudo cp -r ./misc/fonts/* /usr/share/fonts/
+
+
+              array=( https://extensions.gnome.org/extension/3628/arcmenu/
+              https://extensions.gnome.org/extension/5338/aylurs-widgets/ 
+              https://extensions.gnome.org/extension/4679/burn-my-windows/
+              https://extensions.gnome.org/extension/3740/compiz-alike-magic-lamp-effect/
+              https://extensions.gnome.org/extension/307/dash-to-dock/ 
+              https://extensions.gnome.org/extension/5696/one-window-wonderland/ 
+              https://extensions.gnome.org/extension/750/openweather/
+              https://extensions.gnome.org/extension/4356/top-bar-organizer/ 
+              https://extensions.gnome.org/extension/4684/useless-gaps/ 
+              https://extensions.gnome.org/extension/517/caffeine/ 
+              https://extensions.gnome.org/extension/4412/advanced-alttab-window-switcher/
+              https://extensions.gnome.org/extension/1018/text-scaler/ )
+
+              for i in "${array[@]}"
+              do
+                  EXTENSION_ID=$(curl -s $i | grep -oP 'data-uuid="\K[^"]+')
+                  VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=$EXTENSION_ID" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
+                  wget -O ${EXTENSION_ID}.zip "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=$VERSION_TAG"
+                  gnome-extensions install --force ${EXTENSION_ID}.zip
+                  if ! gnome-extensions list | grep --quiet ${EXTENSION_ID}; then
+                      busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${EXTENSION_ID}
+                  fi
+                  gnome-extensions enable ${EXTENSION_ID}
+                  rm ${EXTENSION_ID}.zip
+              done
+
+              ;;
+            "$wm2" | "$wm4")
+              echo "installing qtile-dots"
+              paru -S --needed -noconfirm nerd-fonts-ubuntu-mono
+              pip install psutil
+
+              cd ~/repos/
+              git clone https://github.com/antoniosarosi/dotfiles.git
+              cp -r dotfiles/.config/qtile ~/.config
+              
+              ;;
+
+            "$wm5" | "$wm6")
+              echo "installing dots-hyprland"
+              cd ~/repos/
+              paru -S --needed --noconfirm blueberry brightnessctl cliphist coreutils curl fish foot fuzzel gjs gnome-bluetooth-3.0 gnome-control-center gnome-keyring gobject-introspection grim gtk3 gtk-layer-shell libdbusmenu-gtk3 meson networkmanager npm plasma-browser-integration playerctl polkit-gnome python-pywal ripgrep sassc slurp starship swayidle typescript upower xorg-xrandr webp-pixbuf-loader wget wireplumber wl-clipboard tesseract yad ydotool adw-gtk3-git cava gojq gradience-git hyprland-git hyprpicker-git lexend-fonts-git python-material-color-utilities python-pywal python-poetry python-build python-pillow ttf-material-symbols-variable-git ttf-space-mono-nerd swaylock-effects-git ttf-jetbrains-mono-nerd wayland-idle-inhibitor-git wlogout wlsunset-git
+              sudo usermod -aG video $(whoami)  # if this somehow doesn't work, just replace "$(whoami)" with your username
+              sudo usermod -aG input $(whoami)  # same for this
+
+              git clone --recursive https://github.com/Aylur/ags.git
+              cd ags
+              npm install
+              meson setup build
+              meson install -C build  
+
+              cd .. # Let's not trash your home folder
+              git clone https://github.com/end-4/dots-hyprland.git
+              cd dots-hyprland && git checkout illogical-impulse
+
+              cp -r .config/* ~/.config/
+              cp -r .local/* ~/.local/
+
+              ;;
+        esac
+fi
+  
+
   elif [ "$livvy" == 1 ]; then
-  echo "installing livova dot files"
+  echo "installing livova dot files not supported yet sorry :("
 fi
 
 
